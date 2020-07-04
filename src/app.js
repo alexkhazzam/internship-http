0e2a28482c3f72964587a475875735065667f9d4
alexander khazzam <alexanderkhazzam@gmail.com>
2020-07-04 17:43:20 -0400

    fixing link

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ function sendHttpRequest(method, url) {
 async function navigateServer() {
   const responseData = await sendHttpRequest(
     "GET",
-    `https://api.github.com/search/repositories?q=name:${userInput.value.trim()}&sort=stars&order=des&per_page=100` //100 per page seems to be the max...CPU will blow up--doesn't go more. Need to parse JSON data for every other page until there is no more data left
+    `https://api.github.com/search/repositories?q=${userInput.value.trim()}&sort=stars&order=des&per_page=100` //100 per page seems to be the max...CPU will blow up--doesn't go more. Need to parse JSON data for every other page until there is no more data left
   );
   endModal();
   searchResults.style.display = "block";
@@ -197,8 +197,8 @@ clearBtn.addEventListener("click", () => {
   information.style.display = "none";
 });
 
-userInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
+userInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
     searchBtn.click();
   }
 });
